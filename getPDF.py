c83a339b396ae9d8d0ebb6e95f991d1d9fbee8e4
edem8 <eddieblack159@gmail.com>
2024-06-07 00:14:25 +0000

    chore: update ignore file

--- getPDF.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-def get_pdf(pdf_doc):
-
-    pdf_loader = PyPDFLoader(pdf_doc)
-    pages = pdf_loader.load_and_split()
-
-    return pages
-    
--- getVECTORSDB.py
+++
@@ -1,8 +0,0 @@
-from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
-
-def get_vector_store(chunks):
-    embeddings = HuggingFaceInstructEmbeddings(model_name='hkunlp/instructor-xl')
-    vectorstore =  FAISS.from_texts(texts=chunks, embeddings=embeddings)
-
-    return vectorstore
